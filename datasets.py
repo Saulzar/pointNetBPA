Some bug fixes, network feature parameter, data normalisation

datasets.py
@@ -12,10 +12,12 @@ import sys
 import torchvision.transforms as transforms
 import argparse
 import json
+import random
+import math
 
 
 class PartDataset(data.Dataset):
-    def __init__(self, root, npoints = 2500, classification = False, class_choice = None, train = True):
+    def __init__(self, root, npoints = None, classification = False, class_choice = None, train = True, transform=None):
         self.npoints = npoints
         self.root = root
         # self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')
@@ -23,6 +25,7 @@ class PartDataset(data.Dataset):
         self.categories = ['cast-off', 'impact', 'expirated']
 
         self.classification = classification
+        self.transform = transform
 
         # with open(self.catfile, 'r') as f:
         #     for line in f:
@@ -40,7 +43,7 @@ class PartDataset(data.Dataset):
             # dir_seg = os.path.join(self.root, self.cat[category], 'points_label')
             #print(dir_point, dir_seg)
             fns = sorted(os.listdir(dir_point))
-            
+
             if train:
                 fns = fns[:int(len(fns) * 0.9)]
             else:
@@ -77,8 +80,16 @@ class PartDataset(data.Dataset):
         #resample
         # point_set = point_set[choice, :]
         # seg = seg[choice]
-        point_set = torch.from_numpy(point_set)        
-        # seg = torch.from_numpy(seg)
+
+        point_set = torch.from_numpy(point_set)
+
+        if self.npoints is not None:
+            inds = torch.LongTensor(self.npoints).random_(0, point_set.size(0))
+            point_set = point_set[inds]
+
+        if self.transform is not None:
+            point_set = self.transform(point_set)
+
         cls = torch.from_numpy(np.array([cls]).astype(np.int64))
         if self.classification:
             return point_set, cls
@@ -87,9 +98,82 @@ class PartDataset(data.Dataset):
         return len(self.datapath)
 
 
+
+def normalize(point_set):
+    x, y = point_set[:, 0], point_set[:, 1]
+
+    x.add_(-x.mean().item()).div_(1000)
+    y.add_(-y.mean().item()).div_(1000)
+
+    return point_set
+
+
+def random_log(l, u):
+    return math.exp(random.uniform(math.log(l), math.log(u)))
+
+
+
+def scaling(sx, sy):
+    return torch.Tensor ([
+      [sx, 0, 0],
+      [0, sy, 0],
+      [0, 0, 1]])
+
+def rotation(a):
+    sa = math.sin(a)
+    ca = math.cos(a)
+
+    return torch.Tensor ([
+      [ca, -sa, 0],
+      [sa,  ca, 0],
+      [0,   0, 1]])
+
+def translation(tx, ty):
+    return torch.Tensor ([
+      [1, 0, tx],
+      [0, 1, ty],
+      [0, 0, 1]])
+
+
+
+
+def transform(translation_range=(-0.1, 0.1), scale_range=(0.8, 1.25), rotation_range=(-25, 25)):
+    def f(point_set):
+        point_set = normalize(point_set)
+
+        x, y = point_set[:, 0], point_set[:, 1]
+        cx, cy = x.mean(), y.mean()
+
+        toCentre = translation(-cx, -cy)
+        fromCentre = translation(cx, cy)
+
+        scale = random.uniform(*scale_range)
+        tx, ty = random.uniform(*translation_range), random.uniform(*translation_range)
+
+        flip = 1 if random.uniform(0, 1) > 0.5 else -1
+
+        t = translation(tx, ty)
+        s = scaling(scale * flip, scale)
+
+        r = rotation(random.uniform(*rotation_range))
+        m = fromCentre.mm(r).mm(s).mm(t).mm(toCentre)
+
+        point_set[:, 2].fill_(1)
+        transformed = m.mm(point_set.t()).t()
+
+        transformed[:, 2].fill_(0)
+
+        # print(transformed.mean(0), point_set.mean(0), cx, cy)
+
+        return transformed
+
+
+    return f
+
+
 if __name__ == '__main__':
     print('test')
-    d = PartDataset(root = '/media/cba62/Elements/PointNet_Data',  classification = True)
+    d = PartDataset(root = '../PointNet_Data',  classification = True)
     print(len(d))
     ps, cls = d[1]
     print(ps.size(), ps.type(), cls.size(), cls.type())

pointnet.py
@@ -19,28 +19,34 @@ import torch.nn.functional as F
 
 
 def normalization(n):
-    def id(x):
-        return x
-    return id
+    return nn.BatchNorm1d(n)
+#    return nn.GroupNorm(num_groups = int(n/16), num_channels=n)
+
+
+    # def id(x):
+    #     return x
+    # return id
 
 
 class STN3d(nn.Module):
-    def __init__(self):
+    def __init__(self, n):
         super(STN3d, self).__init__()
-        self.conv1 = torch.nn.Conv1d(3, 64, 1)
-        self.conv2 = torch.nn.Conv1d(64, 128, 1)
-        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.n = n
+
+        self.conv1 = torch.nn.Conv1d(3, n, 1)
+        self.conv2 = torch.nn.Conv1d(n, n*2, 1)
+        self.conv3 = torch.nn.Conv1d(n*2, n*16, 1)
         self.mp1 = torch.nn.AdaptiveMaxPool1d(1)
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 9)
+        self.fc1 = nn.Linear(n*16, n*8)
+        self.fc2 = nn.Linear(n*8, n*4)
+        self.fc3 = nn.Linear(n*4, 9)
         self.relu = nn.ReLU()
 
-        self.bn1 = normalization(64)
-        self.bn2 = normalization(128)
-        self.bn3 = normalization(1024)
-        self.bn4 = normalization(512)
-        self.bn5 = normalization(256)
+        self.bn1 = normalization(n)
+        self.bn2 = normalization(n*2)
+        self.bn3 = normalization(n*16)
+        self.bn4 = normalization(n*8)
+        self.bn5 = normalization(n*4)
 
 
     def forward(self, x):
@@ -49,7 +55,7 @@ class STN3d(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = self.mp1(x)
-        x = x.view(-1, 1024)
+        x = x.view(-1, self.n*16)
 
         x = F.relu(self.bn4(self.fc1(x)))
         x = F.relu(self.bn5(self.fc2(x)))
@@ -60,20 +66,22 @@ class STN3d(nn.Module):
             iden = iden.cuda()
         x = x + iden
 
-        x = x.view(1, 3, 3)
+        x = x.view(-1, 3, 3)
         return x
 
 
 class PointNetfeat(nn.Module):
-    def __init__(self, global_feat = True):
+    def __init__(self, n, global_feat = True):
         super(PointNetfeat, self).__init__()
-        self.stn = STN3d()
-        self.conv1 = torch.nn.Conv1d(3, 64, 1)
-        self.conv2 = torch.nn.Conv1d(64, 128, 1)
-        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
-        self.bn1 = normalization(64)
-        self.bn2 = normalization(128)
-        self.bn3 = normalization(1024)
+        self.n = n
+
+        self.stn = STN3d(n)
+        self.conv1 = torch.nn.Conv1d(3, n, 1)
+        self.conv2 = torch.nn.Conv1d(n, n*2, 1)
+        self.conv3 = torch.nn.Conv1d(n*2, n*16, 1)
+        self.bn1 = normalization(n)
+        self.bn2 = normalization(n*2)
+        self.bn3 = normalization(n*16)
         self.mp1 = torch.nn.AdaptiveMaxPool1d(1)
         self.global_feat = global_feat
     def forward(self, x):
@@ -87,23 +95,26 @@ class PointNetfeat(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.bn3(self.conv3(x))
         x = self.mp1(x)
-        x = x.view(-1, 1024)
+        x = x.view(-1, self.n*16)
         if self.global_feat:
             return x, trans
         else:
-            x = x.view(-1, 1024, 1) #.repeat(1, 1, self.num_points)
+            x = x.view(-1, n*16, 1) #.repeat(1, 1, self.num_points)
             return torch.cat([x, pointfeat], 1), trans
 
 class PointNetCls(nn.Module):
-    def __init__(self, k = 2):
+    def __init__(self, n=16, k = 2):
         super(PointNetCls, self).__init__()
-        self.feat = PointNetfeat(global_feat=True)
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, k)
-        self.bn1 = normalization(512)
-        self.bn2 = normalization(256)
+        self.n = n
+        self.feat = PointNetfeat(n, global_feat=True)
+        self.fc1 = nn.Linear(n*16, n*8)
+        self.fc2 = nn.Linear(n*8, n*4)
+        self.fc3 = nn.Linear(n*4, k)
+        self.bn1 = normalization(n*8)
+        self.bn2 = normalization(n*4)
         self.relu = nn.ReLU()
+
+
     def forward(self, x):
         x, trans = self.feat(x)
         x = F.relu(self.bn1(self.fc1(x)))
@@ -111,30 +122,6 @@ class PointNetCls(nn.Module):
         x = self.fc3(x)
         return F.log_softmax(x, dim=-1), trans
 
-class PointNetDenseCls(nn.Module):
-    def __init__(self, k = 2):
-        super(PointNetDenseCls, self).__init__()
-        self.k = k
-        self.feat = PointNetfeat(global_feat=False)
-        self.conv1 = torch.nn.Conv1d(1088, 512, 1)
-        self.conv2 = torch.nn.Conv1d(512, 256, 1)
-        self.conv3 = torch.nn.Conv1d(256, 128, 1)
-        self.conv4 = torch.nn.Conv1d(128, self.k, 1)
-        self.bn1 = normalization(512)
-        self.bn2 = normalization(256)
-        self.bn3 = normalization(128)
-
-    def forward(self, x):
-        batchsize = x.size()[0]
-        x, trans = self.feat(x)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.conv4(x)
-        x = x.transpose(2,1).contiguous()
-        x = F.log_softmax(x.view(-1,self.k), dim=-1)
-        x = x.view(batchsize, -1, self.k)
-        return x, trans
 
 
 if __name__ == '__main__':

train_classification.py
@@ -13,9 +13,11 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.autograd import Variable
-from datasets import PartDataset
+from datasets import PartDataset, normalize, transform
 from pointnet import PointNetCls
 import torch.nn.functional as F
+
+from tqdm import tqdm
 
 def my_collate(batch):
     data = [item[0] for item in batch]
@@ -25,27 +27,40 @@ def my_collate(batch):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
+parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
 parser.add_argument('--num_points', type=int, default=2500, help='input batch size')
+parser.add_argument('--seed', type=int, default=0, help='random seed')
+
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--nepoch', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--outf', type=str, default='cls',  help='output folder')
 parser.add_argument('--model', type=str, default = '',  help='model path')
+
+parser.add_argument('--base_features', type=int, default=32, help='base feature size in network (pointnet uses 64)')
+
 
 opt = parser.parse_args()
 print (opt)
 
 blue = lambda x:'\033[94m' + x + '\033[0m'
 
-opt.manualSeed = random.randint(1, 10000) # fix seed
-print("Random Seed: ", opt.manualSeed)
-random.seed(opt.manualSeed)
-torch.manual_seed(opt.manualSeed)
 
-dataset = PartDataset(root = '../PointNet_Data', classification = True, npoints = opt.num_points)
+print("Random Seed: ", opt.seed)
+random.seed(opt.seed)
+torch.manual_seed(opt.seed)
+
+
+
+
+dataset = PartDataset(root = '../PointNet_Data', classification = True, npoints = opt.num_points, transform = normalize)
+
+# dataset = PartDataset(root = '../PointNet_Data', classification = True, npoints = opt.num_points,
+#     transform = transform(translation_range=(-0.0, 0.0), scale_range=(0.8, 1.25), rotation_range=(-10, 10)))
+
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
-                                          shuffle=True, num_workers=int(opt.workers))#, collate_fn=my_collate)
+    shuffle=True, num_workers=int(opt.workers))#, collate_fn=my_collate)
 
-test_dataset = PartDataset(root = '../PointNet_Data', classification = True, train = False, npoints = opt.num_points)
+test_dataset = PartDataset(root = '../PointNet_Data', classification = True, train = False, npoints = opt.num_points, transform = normalize)
 testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batchSize,
                                           shuffle=True, num_workers=int(opt.workers) )#,collate_fn=my_collate)
 
@@ -59,50 +74,72 @@ except OSError:
     pass
 
 
-classifier = PointNetCls(k = num_classes)
+classifier = PointNetCls(n = opt.base_features, k = num_classes)
 
 
 if opt.model != '':
     classifier.load_state_dict(torch.load(opt.model))
 
+device = torch.cuda.current_device()
+optimizer = optim.SGD(classifier.parameters(), lr=opt.lr, momentum=0.9)
+#optimizer = optim.Adam(classifier.parameters(), lr=opt.lr)
 
-optimizer = optim.SGD(classifier.parameters(), lr=0.00000001, momentum=0.9)
-# classifier.cuda()
+classifier.to(device)
+classifier.eval()
 
 num_batch = len(dataset)/opt.batchSize
 
+
+def prepare(data):
+    points, target = data
+    return points.transpose(2,1).to(device), target[:,0].to(device)
+
 for epoch in range(opt.nepoch):
 
-    for i, data in enumerate(dataloader, 0):
-        # print(data)
-        points, target = data
+    correct = 0
+    total_loss = 0
+    count = 0
 
-        points, target = Variable(points), Variable(target[:,0])
-        points = points.transpose(2,1)
-        # points, target = points.cuda(), target.cuda()
+    for data in tqdm(dataloader):
+        classifier.train()
+
+        points, target = prepare(data)
         optimizer.zero_grad()
         pred, _ = classifier(points)
 
-        loss = F.nll_loss(pred, target)
-        print(pred.exp(), target, loss.item())
-
+        loss = F.nll_loss(pred, target, reduction='sum')
+        # print(pred.exp(), target, loss.item())
 
         loss.backward()
         optimizer.step()
         pred_choice = pred.data.max(1)[1]
-        correct = pred_choice.eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' %(epoch, i, num_batch, loss.item(),correct.item() / float(opt.batchSize)))
 
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
-            points, target = data
-            points, target = Variable(points), Variable(target[:,0])
-            points = points.transpose(2,1)
+        correct += pred_choice.eq(target.data).sum().item()
+        total_loss += loss.item()
+        count += points.size(0)
+
+    print('[%d] %s loss: %f accuracy: %f' %(epoch, 'train', total_loss / count, correct / count))
+
+    correct = 0
+    total_loss = 0
+    count = 0
+
+    with torch.no_grad():
+        classifier.eval()
+        for data in tqdm(testdataloader):
+
+            points, target = prepare(data)
             # points, target = points.cuda(), target.cuda()
             pred, _ = classifier(points)
-            loss = F.nll_loss(pred, target)
+            loss = F.nll_loss(pred, target, reduction='sum')
             pred_choice = pred.data.max(1)[1]
-            correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' %(epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
+
+            correct += pred_choice.eq(target.data).sum().item()
+
+            total_loss += loss.item()
+            count += points.size(0)
+
+        print('[%d] %s loss: %f accuracy: %f' %(epoch, blue('test'), total_loss  / count, correct / count))
+
 
     torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))